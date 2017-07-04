4fb7b64d76d320713b153aeeeb960882fe8a1884
zhangzhen92 <zz_dazzling@163.com>
2017-07-04 14:18:18 +0800

    Update NextActivity.java

--- app/src/main/java/com/example/zz/androidanimationdemo/NextActivity.java
+++ app/src/main/java/com/example/zz/androidanimationdemo/NextActivity.java
@@ -10,6 +10,5 @@ public class NextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-        Log.i("123321","test");
     }
 }
